84fa4a5ca7038c3c613654e903314ea6f9c12390
Jairo <jairoandres689@gmail.com>
2018-09-08 23:04:31 -0500

    Mi primer COMMIT

---
+++ typescript.js
@@ -0,0 +1,12 @@
+function saluda(nombre) {
+    console.log("HOLA " + nombre.toUpperCase());
+}
+var nombre = {
+    nombre: "jairo"
+};
+saluda(nombre.nombre);
+/*let nombre:string = "Jairo";
+let apellido:string = "Sotelo";
+//alt + 96 ``
+let texto = `Hola, ${nombre} ${apellido}`;
+*/ 
